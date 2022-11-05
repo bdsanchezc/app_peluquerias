212e33a40a3a28492cbf31e791caaa1f0b2f6b53
Bryan Sanchez <bsanchez1507@gmail.com>
2022-11-04 21:02:03 -0500

    Actualización de la maquetación y diseño del módulo Auth

--- src/layout/AuthLayout.jsx
+++ src/layout/AuthLayout.jsx
@@ -1,8 +1,61 @@
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Autoplay, Navigation, Pagination } from "swiper"
+
+import { SlideInfo } from '../components/general';
+
+import 'swiper/css';
+import 'swiper/css/pagination';
+import "swiper/css/navigation";
 
 export const AuthLayout = ({children}) => {
     return (
-        <section className="w-full h-full bg-auth">
-            {children}
+        <section className="w-full h-full flex">
+            <div className="w-1/4 shadow-x relative z-10">
+                {children}
+            </div>
+            
+            <div className="w-3/4 h-full bg-gradient-primary bg-pattern">
+                <Swiper
+                    spaceBetween={30}
+                    centeredSlides={true}
+                    autoplay={{
+                      delay: 2500,
+                      disableOnInteraction: false,
+                    }}
+                    pagination={{ clickable: true }}
+                    navigation={false}
+                    modules={[Autoplay, Pagination, Navigation]}
+                    className="mySwiper h-full"
+                >
+                    <SwiperSlide className='flex items-center'> 
+                        <SlideInfo 
+                            title="Administra tus empleados"
+                            info="Tendrás control de las personas que ayudan en tu establecimiento, podrás agregar o inhabilitar a tus empleados."
+                            pathImage="/admin-empleados.png"
+                            alt="Administración de empleados"
+                            width={600}
+                        />
+                    </SwiperSlide>
+                    <SwiperSlide className='flex flex-col justify-center items-center'>
+                        <SlideInfo 
+                            title="Suscribete y disfruta"
+                            info="Accede a grandes beneficios al suscribirte como cliente preferencial del establecimiento que tu elijas."
+                            pathImage="/collage1.png"
+                            alt="Suscripción"
+                            width={600}
+                        />
+                    </SwiperSlide>
+                    <SwiperSlide className='flex flex-col justify-center items-center'>
+                        <SlideInfo 
+                            title="Agenda tu cita desde cualquier lugar"
+                            info="Agenda tu cita desde tu celular, tu computador portatil estando en cualquier lugar del pais. Siempre tendrás un centro de belleza cerca para atenderte."
+                            pathImage="/collage2.png"
+                            alt="Administración de empleados"
+                            width={350}
+                        />
+                    </SwiperSlide>
+                </Swiper>
+            </div>
         </section>
     )
 }
--- src/pages/auth/RegisterPage.jsx
+++ src/pages/auth/RegisterPage.jsx
@@ -3,88 +3,171 @@ import { InputField } from "../../components/general";
 import { useForm } from "../../hooks";
 
 export const RegisterPage = () => {
-
-  const { name, last_name, email, password, type_user, onInputChange } = useForm({
-    name: '',
-    last_name: '',
-    email: '',
-    password: '',
-    type_user: ''
-  });
+  const { name, last_name, email, password, type_user, onInputChange } =
+    useForm({
+      name: "",
+      last_name: "",
+      email: "",
+      password: "",
+      type_user: "",
+    });
 
   return (
-    <div className="flex justify-center items-center h-full">
-      <div className="bg-white w-full max-w-xs px-6 py-6 rounded-md shadow-md">
-        <h2 className="font-bold text-2xl mb-5">Registrarse</h2>
-        <p>Completa el siguiente formulario para poder registrarte en el sistema.</p>
+    // <div className="flex justify-center items-center h-full">
+    //   <div className="bg-white w-full max-w-xs px-6 py-6 rounded-md shadow-md">
+    //     <h2 className="font-bold text-2xl mb-5">Registrarse</h2>
+    //     <p>Completa el siguiente formulario para poder registrarte en el sistema.</p>
 
-        <form className="mb-5" autoComplete="off">
-          <InputField 
-            label="Nombres"
-            inputName="name"
-            type="text"
-            value={name}
-            onChange={onInputChange}
-          />
+    //     <form className="mb-5" autoComplete="off">
+    //       <InputField
+    //         label="Nombres"
+    //         inputName="name"
+    //         type="text"
+    //         value={name}
+    //         onChange={onInputChange}
+    //       />
 
-          <InputField 
-            label="Apellidos"
-            inputName="last_name"
-            type="text"
-            value={last_name}
-            onChange={onInputChange}
-          />
+    //       <InputField
+    //         label="Apellidos"
+    //         inputName="last_name"
+    //         type="text"
+    //         value={last_name}
+    //         onChange={onInputChange}
+    //       />
 
-          <InputField 
-            label="Correo electrónico"
-            inputName="email"
-            type="email"
-            value={email}
-            onChange={onInputChange}
-          />
+    //       <InputField
+    //         label="Correo electrónico"
+    //         inputName="email"
+    //         type="email"
+    //         value={email}
+    //         onChange={onInputChange}
+    //       />
 
-          <InputField 
-            label="Contraseña"
-            inputName="password"
-            type="password"
-            value={password}
-            onChange={onInputChange}
-          />
+    //       <InputField
+    //         label="Contraseña"
+    //         inputName="password"
+    //         type="password"
+    //         value={password}
+    //         onChange={onInputChange}
+    //       />
 
-          <div className="my-5 w-full">
-            <p className="mb-2 font-bold">¿Qué tipo de usuario eres?</p>
-            <label htmlFor="type_user_admin" className="mr-5">
-              <input 
-                type="radio" 
-                name="type_user" 
-                id="type_user_admin" 
-                value={type_user} 
-                onChange={onInputChange}
-                className="mr-2"
-                />
-              Administrador
-            </label>
-            <label htmlFor="type_user_client">
-              <input 
-                type="radio" 
-                name="type_user" 
-                id="type_user_client" 
-                value={type_user} 
-                onChange={onInputChange}
-                className="mr-2"
-              />
-              Cliente
-            </label>
-          </div>
+    //       <div className="my-5 w-full">
+    //         <p className="mb-2 font-bold">¿Qué tipo de usuario eres?</p>
+    //         <label htmlFor="type_user_admin" className="mr-5">
+    //           <input
+    //             type="radio"
+    //             name="type_user"
+    //             id="type_user_admin"
+    //             value={type_user}
+    //             onChange={onInputChange}
+    //             className="mr-2"
+    //             />
+    //           Administrador
+    //         </label>
+    //         <label htmlFor="type_user_client">
+    //           <input
+    //             type="radio"
+    //             name="type_user"
+    //             id="type_user_client"
+    //             value={type_user}
+    //             onChange={onInputChange}
+    //             className="mr-2"
+    //           />
+    //           Cliente
+    //         </label>
+    //       </div>
 
-          <button type="submit" className="bg-primary py-3 px-4 rounded-md">Registrarse</button>
-        </form>
+    //       <button type="submit" className="bg-primary py-3 px-4 rounded-md">Registrarse</button>
+    //     </form>
 
-        <h4 className="font-bold mb-2">¿Ya posees una cuenta?</h4>
-        <p className="mb-5">Si deseas iniciar sesión, haz clic en el siguiente enlace.</p>
-        <Link to="/auth/login" className="color-primary font-bold">Iniciar sesión</Link>
+    //     <h4 className="font-bold mb-2">¿Ya posees una cuenta?</h4>
+    //     <p className="mb-5">Si deseas iniciar sesión, haz clic en el siguiente enlace.</p>
+    //     <Link to="/auth/login" className="color-primary font-bold">Iniciar sesión</Link>
+    //   </div>
+    // </div>
+
+    <div className="w-full h-full flex flex-col justify-center px-10">
+      <div className="py-6 mb-5">
+        <img src="/logo.svg" alt="styled spapp" className="w-52" />
       </div>
+
+      <h2 className="font-bold text-2xl mb-5">Iniciar sesión</h2>
+      <p>
+        Bienvenido a styled spapp, para acceder al sistema utiliza las
+        credenciales creadas anteriormente.
+      </p>
+
+      <form className="mb-5" autoComplete="off">
+        <InputField
+          label="Nombres"
+          inputName="name"
+          type="text"
+          value={name}
+          onChange={onInputChange}
+        />
+
+        <InputField
+          label="Apellidos"
+          inputName="last_name"
+          type="text"
+          value={last_name}
+          onChange={onInputChange}
+        />
+
+        <InputField
+          label="Correo electrónico"
+          inputName="email"
+          type="email"
+          value={email}
+          onChange={onInputChange}
+        />
+
+        <InputField
+          label="Contraseña"
+          inputName="password"
+          type="password"
+          value={password}
+          onChange={onInputChange}
+        />
+
+        <div className="my-5 w-full">
+          <p className="mb-2 font-bold">¿Qué tipo de usuario eres?</p>
+          <label htmlFor="type_user_admin" className="mr-5">
+            <input
+              type="radio"
+              name="type_user"
+              id="type_user_admin"
+              value={type_user}
+              onChange={onInputChange}
+              className="mr-2"
+            />
+            Administrador
+          </label>
+          <label htmlFor="type_user_client">
+            <input
+              type="radio"
+              name="type_user"
+              id="type_user_client"
+              value={type_user}
+              onChange={onInputChange}
+              className="mr-2"
+            />
+            Cliente
+          </label>
+        </div>
+
+        <button type="submit" className="bg-primary py-3 px-4 rounded-md">
+          Registrarse
+        </button>
+      </form>
+
+      <h4 className="font-bold mb-2">¿Ya tienes una cuenta?</h4>
+      <p className="mb-5">
+        Si ya tienes una cuenta, haz clic en el siguiente enlace para iniciar sesión
+      </p>
+      <Link to="/auth" className="color-primary font-bold">
+        Ir a inicio de sesión
+      </Link>
     </div>
   );
-
-}
+};
